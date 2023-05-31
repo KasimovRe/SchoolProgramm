196853f54071b224bbb3beae49e110e184d17e44
kasimovvladlen2006rez@gmail.com <kasimovvladlen2006rez@gmail.com>
2023-05-31 20:54:41 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProgramm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Находим наименьшую цифру в числе n
+            int minDigit = 9;
+            int temp = n;
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                if (digit < minDigit)
+                {
+                    minDigit = digit;
+                }
+                temp /= 10;
+            }
+
+            // Находим количество наименьших цифр в числе n
+            int count = 0;
+            temp = n;
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                if (digit == minDigit)
+                {
+                    count++;
+                }
+                temp /= 10;
+            }
+
+            Console.WriteLine("Наименьшая цифра в числе: " + minDigit);
+            Console.WriteLine("Количество наименьших цифр в числе: " + count);
+
+            Console.ReadKey();
+        }
+    }
+}
